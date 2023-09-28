03f43c5355833fd62745a5f9863ccd09f9047d4e
Brandon <oallelsefailo@gmail.com>
2023-09-27 20:48:08 -0700

    Added footer / added routes

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ".footer.css";
+
+function Header() {
+  return (
+<footer>
+    <p>lorem
+        
+    </p>
+</footer>
+  );
+}
+
+export default Footer;
