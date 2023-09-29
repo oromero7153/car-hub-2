4487b2066a90e010ecd502a3db4665c4d54ea174
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-09-29 07:10:51 -0700

    added some filler content

--- src/components/Rides_expanded.js
+++ src/components/Rides_expanded.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Footer from "./Footer"
+import Header from "./Header"
 import { Link } from "react-router-dom";
 
 function ridesExpanded () {
     return (
+        <div>
         <main>
+            <header></header>
             <h1></h1>
+            <footer></footer>
         </main>
+        </div>
     )
 };
 
-module.exports = ridesExpanded
-
+export default ridesExpanded;
