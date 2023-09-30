4e93556697a67e5817205865eb8b936214de8630
Jose Rodriguez <joserodriguez@Joses-MBP-2.home>
2023-09-30 12:01:00 -0700

    updates

--- src/components/EventsPage.js
+++ src/components/EventsPage.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { EventDates } from "./EventDates";
@@ -6,7 +7,7 @@ import { EventDates } from "./EventDates";
 export default function EventsPage () {
     return (
         <div>
-            
+            <h1>Hello</h1>
         </div>
     );
 }
