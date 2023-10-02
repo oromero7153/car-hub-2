e22a330e78577aafa90e7faeb9d86b73aa7ed670
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-10-02 07:54:30 -0700

    remove import from rides_expanded

--- src/components/Rides_expanded.js
+++ src/components/Rides_expanded.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./ridesExpanded.css"
 import maserati from "../assets/rides_expanded/2023-mc20.jpg"
-import Update from "./update";
 import { useNavigate } from "react-router-dom";
 
 function RidesExpanded() {
