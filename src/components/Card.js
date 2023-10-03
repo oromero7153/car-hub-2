89cb735604702698609b539e6fd260409a2e443c
Brandon <oallelsefailo@gmail.com>
2023-10-02 23:17:29 -0700

    Fixed grid layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
+import React from "react";
+import "./card.css";
+
 export default function Card(props) {
-    return (
-        <><div style={{border: "1px solid black", borderRadius: "8px", width: "200px", height: "250px", margin: "15px", boxShadow: "0px 0px 5px #000000"}}>
-            <span>{props.eventName}</span>
-            <div style ={{height: "10px", textAlign: "center"}}>{props.date}</div>
-                <img src={props.imageURL} alt="" style= {{height: "100px", textAlign:"center", margin: '17px'}}/> 
-            <div style={{width: "100%", textAlign: "center"}}> {props.location}
-            </div> 
-            </div> 
-            </>
-    )
+  return (
+    <div className="card">
+      <span className="event-name">{props.eventName}</span>
+      <div className="event-date">{props.date}</div>
+      <img src={props.imageURL} alt="" className="event-image" />
+      <div className="event-location">{props.location}</div>
+    </div>
+  );
 }
--- src/components/EventsPage.js
+++ src/components/EventsPage.js
@@ -4,32 +4,38 @@ import Footer from "./Footer";
 import { EventDates } from "./EventDates";
 import Card from "./Card";
 import { useNavigate } from "react-router-dom";
+import "./eventspage.css";
 
-export default function EventsPage (props) {
-    
-    const navigate= useNavigate();
+export default function EventsPage(props) {
+  const navigate = useNavigate();
 
-    return (
-        <><div style= {{display: "flex", flexWrap: "wrap"}}> 
-            {EventDates.map((element, index) =>
-            <Card  
-            key= {index} 
-            imageURL= {element.imageURL} 
-            eventName={element.eventName} 
+  return (
+    <div className="events-page">
+      <div className="content-grid">
+        {EventDates.map((element, index) => (
+          <Card
+            key={index}
+            imageURL={element.imageURL}
+            eventName={element.eventName}
             location={element.location}
-            date={element.date} 
-            />)
-        }
-        </div>
+            date={element.date}
+          />
+        ))}
+      </div>
+
+      <div className="update-events">
         <div>
-            <p>Stay up to date with events! New events will be added soon!</p>
+          <p>Stay up to date with events! New events will be added soon!</p>
         </div>
 
         <div className="button">
-            <p>You can also add your own events here!</p>
-                <button onClick={() => navigate('/events/AddEvent')} >Add Event</button>
-                <button>Delete Event</button>
+          <p>You can also add your own events here!</p>
+          <button onClick={() => navigate("/events/AddEvent")}>
+            Add Event
+          </button>
+          <button>Delete Event</button>
         </div>
-        </>
-    );
+      </div>
+    </div>
+  );
 }
