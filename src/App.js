ef9872771cbf5f0e37d639759312fee11e48eb09
Manny Delgado <mannygdp@gmail.com>
2023-10-04 00:31:29 -0700

    fixed route

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Header from "./components/Header";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
-// import RidesExpanded from "./components/Rides_expanded";
-// import Update from "./components/update"
+import RidesExpanded from "./components/Rides_expanded";
+import Update from "./components/update"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutUs from "./components/aboutUs";
 
@@ -14,9 +14,9 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          {/* <Route path="/" element={<Container />} /> */}
-          {/* <Route path="/rides" element={<Rides />} />
-          <Route path="/events" element={<Events />} /> */}
+          <Route path="/" element={<Container />} />
+          {/* <Route path="/rides" element={<Rides />} /> */}
+          {/* <Route path="/events" element={<Events />} /> */}
           <Route path="/aboutUs" element={<AboutUs />} />
         </Routes>
         <Footer />
