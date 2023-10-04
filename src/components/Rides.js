b47f104282600c325e1da723389b0dfa5fd04ee2
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-10-04 11:01:55 -0700

    commit

--- src/components/Rides.js
+++ src/components/Rides.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 function Rides() {
+
+    const [photo, setPhoto] = useState('')
+    const [brand, setBrand] = useState('')
+    const [model, setModel] = useState('')
+    const [year, setYear] = useState('')
+    const [type, setType] = useState('')
+    const [engine, setEngine] = useState('')
+    const [drivetrain, setDrivetrain] = useState('')
+    const [owner, setOwner] = useState('')
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log(photo + '|' + brand + '|' + model + '|' + year + '|' + type + '|' + engine + '|' + drivetrain + '|' + owner)
+
+    }
+
     return (
         <div>
             <main>
@@ -8,38 +27,38 @@ function Rides() {
                 <form method="POST" action="/rides">
                     <div>
                         <label>Photos: </label>
-                        <input id='photos' name='photos' />
+                        <input id='photos' name='photos' value= {photo} onChange={ (e)=> setPhoto(e.target.value)} />
                     </div>
                     <div>
                         <label>Brand: </label>
-                        <input id='brand' name='brand' required />
+                        <input id='brand' name='brand' value= {brand} onChange={ (e)=> setBrand(e.target.value)} required />
                     </div>
                     <div>
                         <label>Model: </label>
-                        <input id='model' name='model' required />
+                        <input id='model' name='model' value= {model} onChange={ (e)=> setModel(e.target.value)} required />
                     </div>
                     <div>
                         <label>Year: </label>
-                        <input id='year' name='year' required />
+                        <input id='year' name='year' value= {year} onChange={ (e)=> setYear(e.target.value)} required />
                     </div>
                     <div>
                         <label>Type: </label>
-                        <input id='type' name='type' required />
+                        <input id='type' name='type' value= {type} onChange={ (e)=> setType(e.target.value)} required />
                     </div>
                     <div>
                         <label>Engine: </label>
-                        <input id='engine' name='engine' required />
+                        <input id='engine' name='engine' value= {engine} onChange={ (e)=> setEngine(e.target.value)} required />
                     </div>
                     <div>
                         <label>Drivetrain: </label>
-                        <input id='drivetrain' name='drivetrain' required />
+                        <input id='drivetrain' name='drivetrain' value= {drivetrain} onChange={ (e)=> setDrivetrain(e.target.value)} required />
                     </div>
                     <div>
                         <label>Owner: </label>
-                        <input id='owner' name='owner' required />
+                        <input id='owner' name='owner' value= {owner} onChange={ (e)=> setOwner(e.target.value)} required />
                     </div>
                 </form>
-                <input type="submit" value="Add Ride" />
+                <input type="submit" onClick={handleSubmit} />
             </main>
         </div>
     )
