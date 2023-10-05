5d0ff3de07177d1fbcdae7afd55cc7d3fd89f3f3
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-10-05 08:15:39 -0700

    new backend folder

--- server/models/schemas.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const addRidesSchema = {
-    brand: String,
-    model: String,
-}
- const Rides = mongoose.model("Ride", addRidesSchema);
-
- module.exports= Rides;
