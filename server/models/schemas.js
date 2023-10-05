6d66bbb42a782927203e3e68f7450114c307008a
Brandon <oallelsefailo@gmail.com>
2023-10-04 23:06:54 -0700

    Finally got DB working and responsive

--- server/models/schemas.js
+++ server/models/schemas.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const ridesSchema = new Schema({
- photo: { type: String},  
- brand: { type: String, required: true },
- model: { type: String, required: true},
- year: { type: Number, required: true},
- type: { type: String, required: true},
- engine: { type: String, required: true },
- drivetrain: {type: String, required: true},
- owner:  {type: String, required: true},
+  photo: { type: String },
+  brand: { type: String, required: true },
+  model: { type: String, required: true },
+  year: { type: String, required: true },
+  type: { type: String, required: true },
+  engine: { type: String, required: true },
+  drivetrain: { type: String, required: true },
+  owner: { type: String, required: true },
 });
 
-const Rides = mongoose.model('Rides', ridesSchema, 'rides');
+const Rides = mongoose.model("Rides", ridesSchema, "rides");
 
-const mySchemas = {'Rides': Rides}
+const mySchemas = { Rides: Rides };
 
-module.exports = mySchemas
+module.exports = mySchemas;
