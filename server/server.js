433ab0cdfe70f2193a54893f9aeece94226522e3
Brandon <oallelsefailo@gmail.com>
2023-10-04 19:52:03 -0700

    Added routes, models NOTE: Rides.js is temporarily in place to test the DB

--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
-require('dotenv/config');
+require("dotenv").config();
 
-const mongoose = require('mongoose')
-const express = require('express');
-const cors = require('cors')
-const bodyParser = require('body-parser');
-const router = require('./routes/routes')
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const router = require("./routes/routes");
 
-const app = express('express');
+const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const corsOptions = {
-    origin: '*',
-    credentials: true, 
-    optionSuccessStatus: 200
-}
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.use('/rides', router)
+app.use("/rides", router);
 
-const dbOptions = {useNewUrlPArser: true, useUnifiedTopology: true}
-mongoose.connect(process.env.DB_URI, dbOptions)
+const dbOptions = { useNewUrlPArser: true, useUnifiedTopology: true };
+mongoose.connect(process.env.MONGO_URI, dbOptions);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
